e4c791494df4458fe13c2dfdf4cd98c1790b1505
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-08-23 18:30:16 -0300

    Refactor ModCore for improved mod handling

--- source/ModCore.hx
+++ source/ModCore.hx
@@ -4,28 +4,53 @@ import polymod.backends.PolymodAssets.PolymodAssetType;
 import polymod.format.ParseRules.LinesParseFormat;
 import polymod.format.ParseRules.TextFileFormat;
 import polymod.Polymod;
+import polymod.Polymod.PolymodError;
+import polymod.Polymod.ModMetadata;
 #end
 
-/**
- * Okay now this is epic.
- */
+#if mobile
+import mobile.backend.StorageUtil;
+#end
+
 class ModCore
 {
-	/**
-	 * The current API version.
-	 * Must be formatted in Semantic Versioning v2; <MAJOR>.<MINOR>.<PATCH>.
-	 * 
-	 * Remember to increment the major version if you make breaking changes to mods!
-	 */
 	static final API_VERSION = "0.1.0";
 
-	static final MOD_DIRECTORY = "mods";
+	static final MOD_DIRECTORY_NAME = "mods";
+
+	static var initialized:Bool = false;
+
+	#if FEATURE_MODCORE
+	static var loadedModMetadata:Array<ModMetadata> = [];
+	#end
+
+	public static var modRoot(get, never):String;
+
+	static function get_modRoot():String
+	{
+		#if mobile
+		return StorageUtil.resolvePath(MOD_DIRECTORY_NAME);
+		#else
+		return MOD_DIRECTORY_NAME;
+		#end
+	}
 
 	public static function initialize()
 	{
 		#if FEATURE_MODCORE
+		if (initialized)
+		{
+			Debug.logWarn("ModCore already initialized, ignoring duplicate call.");
+			return;
+		}
+
+		#if mobile
+		StorageUtil.createDirectory(MOD_DIRECTORY_NAME);
+		#end
+
 		Debug.logInfo("Initializing ModCore...");
 		loadModsById(getModIds());
+		initialized = true;
 		#else
 		Debug.logInfo("ModCore not initialized; not supported on this platform.");
 		#end
@@ -35,78 +60,84 @@ class ModCore
 	public static function loadModsById(ids:Array<String>)
 	{
 		Debug.logInfo('Attempting to load ${ids.length} mods...');
-		var loadedModList = polymod.Polymod.init({
-			// Root directory for all mods.
-			modRoot: MOD_DIRECTORY,
-			// The directories for one or more mods to load.
+
+		var loadedModList = Polymod.init({
+			modRoot: modRoot,
 			dirs: ids,
-			// Framework being used to load assets. We're using a CUSTOM one which extends the OpenFL one.
 			framework: CUSTOM,
-			// The current version of our API.
 			apiVersion: API_VERSION,
-			// Call this function any time an error occurs.
 			errorCallback: onPolymodError,
-			// Enforce semantic version patterns for each mod.
-			// modVersions: null,
-			// A map telling Polymod what the asset type is for unfamiliar file extensions.
-			// extensionMap: [],
-
 			frameworkParams: buildFrameworkParams(),
-
-			// Use a custom backend so we can get a picture of what's going on,
-			// or even override behavior ourselves.
 			customBackend: ModCoreBackend,
-
-			// List of filenames to ignore in mods. Use the default list to ignore the metadata file, etc.
 			ignoredFiles: Polymod.getDefaultIgnoreList(),
-
-			// Parsing rules for various data formats.
 			parseRules: buildParseRules(),
 		});
+
+		if (loadedModList == null)
+		{
+			Debug.logError("Polymod failed to initialize; mods will not be active.");
+			loadedModMetadata = [];
+			return;
+		}
+
+		loadedModMetadata = loadedModList;
 
 		Debug.logInfo('Mod loading complete. We loaded ${loadedModList.length} / ${ids.length} mods.');
 
 		for (mod in loadedModList)
-			Debug.logTrace('  * ${mod.title} v${mod.modVersion} [${mod.id}]');
+			Debug.logInfo('  * ${mod.title} v${mod.modVersion} [${mod.id}]');
 
-		var fileList = Polymod.listModFiles("IMAGE");
-		Debug.logInfo('Installed mods have replaced ${fileList.length} images.');
-		for (item in fileList)
-			Debug.logTrace('  * $item');
+		logReplacedAssets("IMAGE");
+		logReplacedAssets("TEXT");
+		logReplacedAssets("MUSIC");
+		logReplacedAssets("SOUND");
+	}
 
-		fileList = Polymod.listModFiles("TEXT");
-		Debug.logInfo('Installed mods have replaced ${fileList.length} text files.');
-		for (item in fileList)
-			Debug.logTrace('  * $item');
+	static function logReplacedAssets(type:String):Void
+	{
+		var fileList = Polymod.listModFiles(type);
+		Debug.logInfo('Installed mods have replaced ${fileList.length} $type files.');
+	}
 
-		fileList = Polymod.listModFiles("MUSIC");
-		Debug.logInfo('Installed mods have replaced ${fileList.length} music files.');
-		for (item in fileList)
-			Debug.logTrace('  * $item');
+	public static function reload():Void
+	{
+		Debug.logInfo("Reloading mods...");
+		unload();
+		loadModsById(getModIds());
+	}
 
-		fileList = Polymod.listModFiles("SOUND");
-		Debug.logInfo('Installed mods have replaced ${fileList.length} sound files.');
-		for (item in fileList)
-			Debug.logTrace('  * $item');
+	public static function unload():Void
+	{
+		if (!initialized)
+			return;
+
+		Polymod.clearCache();
+		loadedModMetadata = [];
+		Debug.logInfo("Mods unloaded.");
+	}
+
+	public static function getLoadedMods():Array<ModMetadata>
+	{
+		return loadedModMetadata;
+	}
+
+	public static function getAvailableMods():Array<ModMetadata>
+	{
+		return Polymod.scan(modRoot);
 	}
 
 	static function getModIds():Array<String>
 	{
-		Debug.logInfo('Scanning the mods folder...');
-		var modMetadata = Polymod.scan(MOD_DIRECTORY);
+		Debug.logInfo('Scanning the mods folder at $modRoot...');
+		var modMetadata = Polymod.scan(modRoot);
 		Debug.logInfo('Found ${modMetadata.length} mods when scanning.');
-		var modIds = [for (i in modMetadata) i.id];
-		return modIds;
+		return [for (i in modMetadata) i.id];
 	}
 
 	static function buildParseRules():polymod.format.ParseRules
 	{
 		var output = polymod.format.ParseRules.getDefault();
-		// Ensure TXT files have merge support.
 		output.addType("txt", TextFileFormat.LINES);
-
-		// You can specify the format of a specific file, with file extension.
-		// output.addFile("data/introText.txt", TextFileFormat.LINES)
 		return output;
 	}
 
@@ -114,44 +145,46 @@ class ModCore
 	{
 		return {
 			assetLibraryPaths: [
-				"default" => "./preload", // ./preload
-				"sm" => "./sm", "songs" => "./songs", "shared" => "./", "tutorial" => "./tutorial",
-				"week1" => "./week1", "week2" => "./week2", "week3" => "./week3", "week4" => "./week4", "week5" => "./week5", "week6" => "./week6"
+				"default" => "./preload",
+				"sm" => "./sm",
+				"songs" => "./songs",
+				"shared" => "./",
+				"tutorial" => "./tutorial",
+				"week1" => "./week1",
+				"week2" => "./week2",
+				"week3" => "./week3",
+				"week4" => "./week4",
+				"week5" => "./week5",
+				"week6" => "./week6"
 			]
 		}
 	}
 
 	static function onPolymodError(error:PolymodError):Void
 	{
-		// Perform an action based on the error code.
 		switch (error.code)
 		{
-			// case "parse_mod_version":
-			// case "parse_api_version":
-			// case "parse_mod_api_version":
-			// case "missing_mod":
-			// case "missing_meta":
-			// case "missing_icon":
-			// case "version_conflict_mod":
-			// case "version_conflict_api":
-			// case "version_prerelease_api":
-			// case "param_mod_version":
-			// case "framework_autodetect":
-			// case "framework_init":
-			// case "undefined_custom_backend":
-			// case "failed_create_backend":
-			// case "merge_error":
-			// case "append_error":
+			case "missing_mod":
+				Debug.logWarn('A mod folder listed for loading was not found: ${error.message}');
+			case "missing_meta":
+				Debug.logWarn('A mod is missing its _polymod_meta.json file: ${error.message}');
+			case "missing_icon":
+				Debug.logWarn('A mod is missing its icon file: ${error.message}');
+			case "version_conflict_mod", "version_conflict_api":
+				Debug.logError('Mod/API version mismatch: ${error.message}');
+			case "framework_init", "failed_create_backend", "undefined_custom_backend":
+				Debug.logError('Polymod failed to initialize the asset backend: ${error.message}');
+			case "merge_error", "append_error":
+				Debug.logWarn('Failed to merge a mod data file: ${error.message}');
 			default:
-				// Log the message based on its severity.
 				switch (error.severity)
 				{
 					case NOTICE:
-						Debug.logInfo(error.message, null);
+						Debug.logInfo(error.message);
 					case WARNING:
-						Debug.logWarn(error.message, null);
+						Debug.logWarn(error.message);
 					case ERROR:
-						Debug.logError(error.message, null);
+						Debug.logError(error.message);
 				}
 		}
 	}
@@ -164,36 +197,30 @@ class ModCoreBackend extends OpenFLBackend
 	public function new()
 	{
 		super();
-		Debug.logTrace('Initialized custom asset loader backend.');
 	}
 
 	public override function clearCache()
 	{
 		super.clearCache();
-		Debug.logWarn('Custom asset cache has been cleared.');
 	}
 
 	public override function exists(id:String):Bool
 	{
-		Debug.logTrace('Call to ModCoreBackend: exists($id)');
 		return super.exists(id);
 	}
 
 	public override function getBytes(id:String):lime.utils.Bytes
 	{
-		Debug.logTrace('Call to ModCoreBackend: getBytes($id)');
 		return super.getBytes(id);
 	}
 
 	public override function getText(id:String):String
 	{
-		Debug.logTrace('Call to ModCoreBackend: getText($id)');
 		return super.getText(id);
 	}
 
 	public override function list(type:PolymodAssetType = null):Array<String>
 	{
-		Debug.logTrace('Listing assets in custom asset cache ($type).');
 		return super.list(type);
 	}
 }
