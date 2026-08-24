a6c6f72b724b7e790e2d6c6a06dbf1a1d89c5b46
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-08-24 13:06:25 -0300

    Refactor Discord client initialization and presence update

--- source/Discord.hx
+++ source/Discord.hx
@@ -3,35 +3,50 @@ package;
 #if FEATURE_DISCORD
 import Sys.sleep;
 import discord_rpc.DiscordRpc;
+import sys.thread.Thread;
 
 using StringTools;
 
 class DiscordClient
 {
+	public static var clientID:String = "557069829501091850";
+	private static var isInitialized:Bool = false;
+
 	public function new()
 	{
-		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "557069829501091850", // change this to what ever the fuck you want lol
+			clientID: clientID,
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
 		});
-		trace("Discord Client started.");
 
 		while (true)
 		{
 			DiscordRpc.process();
 			sleep(2);
-			// trace("Discord Client Update");
 		}
 
 		DiscordRpc.shutdown();
 	}
 
+	public static function initialize()
+	{
+		if (isInitialized) return;
+
+		isInitialized = true;
+		Thread.create(() ->
+		{
+			new DiscordClient();
+		});
+	}
+
 	public static function shutdown()
 	{
+		if (!isInitialized) return;
+
 		DiscordRpc.shutdown();
+		isInitialized = false;
 	}
 
 	static function onReady()
@@ -40,34 +55,19 @@ class DiscordClient
 			details: "In the Menus",
 			state: null,
 			largeImageKey: 'icon',
-			largeImageText: "fridaynightfunkin"
+			largeImageText: "Friday Night Funkin'"
 		});
 	}
 
-	static function onError(_code:Int, _message:String)
-	{
-		trace('Error! $_code : $_message');
-	}
+	static function onError(_code:Int, _message:String) {}
 
-	static function onDisconnected(_code:Int, _message:String)
-	{
-		trace('Disconnected! $_code : $_message');
-	}
-
-	public static function initialize()
-	{
-		var DiscordDaemon = sys.thread.Thread.create(() ->
-		{
-			new DiscordClient();
-		});
-		trace("Discord Client initialized");
-	}
+	static function onDisconnected(_code:Int, _message:String) {}
 
 	public static function changePresence(details:String, state:Null<String>, ?smallImageKey:String, ?hasStartTimestamp:Bool, ?endTimestamp:Float)
 	{
-		var startTimestamp:Float = if (hasStartTimestamp) Date.now().getTime() else 0;
+		var startTimestamp:Float = hasStartTimestamp ? Date.now().getTime() : 0;
 
-		if (endTimestamp > 0)
+		if (endTimestamp != null && endTimestamp > 0)
 		{
 			endTimestamp = startTimestamp + endTimestamp;
 		}
@@ -76,14 +76,11 @@ class DiscordClient
 			details: details,
 			state: state,
 			largeImageKey: 'icon',
-			largeImageText: "fridaynightfunkin",
+			largeImageText: "Friday Night Funkin'",
 			smallImageKey: smallImageKey,
-			// Obtained times are in milliseconds so they are divided so Discord can use it
-			startTimestamp: Std.int(startTimestamp / 1000),
-			endTimestamp: Std.int(endTimestamp / 1000)
+			startTimestamp: startTimestamp > 0 ? Std.int(startTimestamp / 1000) : null,
+			endTimestamp: (endTimestamp != null && endTimestamp > 0) ? Std.int(endTimestamp / 1000) : null
 		});
-
-		// trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
 	}
 }
 #end
